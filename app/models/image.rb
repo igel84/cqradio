3028ce278c82860d046f8a803977009daf70529f
igel <igel.yulian@mail.ru>
2011-12-06 12:45:05 +0400

    add_format_to_image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :product
-  has_attached_file :photo, :styles => { :thumb=> "100x100#", :small  => "250x250>", :normal => "600x600>" }
+  has_attached_file :photo, :styles => {
+  	:thumb=> ["100x100#", :jpg], :small  => ["250x250>", :jpg], :normal => ["600x600>", :jpg]
+  	#,:preview => { :processors => [:watermark], :geometry => '300x300>', :watermark_path => "#{Rails.root}/public/images/watermark.png", :position => 'Center', :format =>:jpg, :convert_options => ['image/jpeg'] }
+  }
 end
