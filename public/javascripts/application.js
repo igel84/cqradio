2cbdeaa6f093da2d5dcdbe0e6b1861e357911ea3
igel <igel.yulian@mail.ru>
2011-12-13 21:45:19 +0400

    update 13.12

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
+	//древовидное меню
+	$(".menu-product").treeview(
+		{ persist: "location", collapsed: true, unique: true, animated: "fast", persist: "cookie"
+			//,toggle: function() { alert(this + " переключено!"); }
+		}
+	);
+
+	//лента
+	var div = $('div.sc_menu'),
+		ul = $('ul.sc_menu'),
+		ulPadding = 15;
+
+	//Get menu width
+	var divWidth = div.width();
+
+	//Remove scrollbars
+	div.css({overflow: 'hidden'});
+
+	//Find last image container
+	var lastLi = ul.find('li:last-child');
+
+	//When user move mouse over menu
+	div.mousemove(function(e){
+		//As images are loaded ul width increases,
+		//so we recalculate it each time
+		var offset = lastLi.offset();
+		var ulWidth = offset.left + lastLi.outerWidth() + ulPadding;
+		var left = (e.pageX - div.offset().left) * (ulWidth-divWidth) / divWidth;
+		div.scrollLeft(left);
+	});
+
 	$('#find-but').click(function(){
 		var text = $('#find-text').val();
-		window.location = 'http://localhost:3000/find/' + text;
+		window.location = 'http://cqradio.igel84.locum.ru/find/' + text;
 		return false;
 	});
 
